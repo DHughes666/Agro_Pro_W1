37847b92aae2e17377a144226eaf9221e62d8f92
ddkkdss <labalaba@email.com>
2023-03-19 17:09:03 +0100

    Initial commit

---
+++ agroProWorking_1/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for agroProWorking_1 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'agroProWorking_1.settings')
+
+application = get_wsgi_application()
